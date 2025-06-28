d874ea851e726d05cf827e8bbcc718db9422f11a
asadmukhtarr <axad03213@gmail.com>
2025-06-28 18:21:36 +0500

    view with controller

---
+++ shopping/app/Http/Controllers/pagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pagesController extends Controller
+{
+    // home ..
+    public function index(){
+        return view('welcome');
+    }
+    // about ..
+    public function about(){
+        return view('about');
+    }
+    // contact ..
+    public function contact(){
+        return view('contact');
+    }
+    // products ..
+    public function products(){
+        return view('products');
+    }
+}
--- shopping/routes/web.php
+++ shopping/routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pagesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// home ..
+Route::get('/', [pagesController::class,'index'])->name('home');
+// about ...
+Route::get('/about',[pagesController::class,'about'])->name('about');
+// contact ...
+Route::get('/contact',[pagesController::class,'contact'])->name('contact');
+// products ..
+Route::get('/products',[pagesController::class,'products'])->name('products');
+
+
 Route::get('/atif',function(){
     return "Hello Atif";
 });
